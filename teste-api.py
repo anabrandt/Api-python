98b9a7d97f991953aa3a380d55cfa5aa82a4d32b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-28 11:46:16 -0300

    listapi

--- teste-api.py
+++ teste-api.py
@@ -1,5 +1,4 @@
-
-
+import db
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -7,5 +6,14 @@ app = Flask(__name__)
 @app.route("/hello")  
 def hello():
     return {"msg":"Hello Wordl!!"}
+
+@app.route("/carros", methods=['GET'])
+def get_all():
+    return db.carros
+
+def get_id(id):
+    for car in db.carros:
+        if car['id'] == id:
+            return car 
 
 app.run(debug=True)
